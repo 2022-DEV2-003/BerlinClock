f01356241710477bc564aa098ce0235e20ccd8f9
2022-DEV2-003 <2022-DEV2-003@test.com>
2022-05-07 19:37:33 +0200

    display BerlinClock state in MainActivity

--- app/src/main/java/com/example/berlinclock/MainActivity.kt
+++ app/src/main/java/com/example/berlinclock/MainActivity.kt
@@ -3,13 +3,14 @@ package com.example.berlinclock
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.berlinclock.ui.BerlinClock
 import com.example.berlinclock.ui.theme.BerlinClockTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,22 +23,12 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    Box(modifier = Modifier.padding(16.dp))
+                    {
+                        BerlinClock()
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    BerlinClockTheme {
-        Greeting("Android")
     }
 }
