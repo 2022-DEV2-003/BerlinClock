90c95cabde9ecc723b167fcc5a283635524ff63c
2022-DEV2-003 <2022-DEV2-003@test.com>
2022-05-07 19:33:45 +0200

    implement BerlinClockStateConverter and fix tests

--- app/src/main/java/com/example/berlinclock/converters/BerlinClockStateConverter.kt
+++ app/src/main/java/com/example/berlinclock/converters/BerlinClockStateConverter.kt
@@ -8,7 +8,13 @@ import java.time.LocalDateTime
  * A component capable of converting an instance of [LocalDateTime] into an instance of [BerlinClockState]
  */
 object BerlinClockStateConverter : Converter<LocalDateTime, BerlinClockState> {
-    override fun convert(source: LocalDateTime): BerlinClockState {
-        TODO("Not yet implemented")
-    }
+
+    override fun convert(source: LocalDateTime) = convertToBerlinClockState(source)
+
+    private fun convertToBerlinClockState(time: LocalDateTime) =
+        BerlinClockState(
+            time.second % 2 == 0,
+            time.hour / 5, time.hour % 5,
+            time.minute / 5, time.minute % 5
+        )
 }
